12931dd82b34332643ef3ee99d39e35e8cd55806
at-Ahmadullah <142923365+at-Ahmadullah@users.noreply.github.com>
2024-01-14 22:29:38 +0530

    Add files via upload

---
+++ booksky/script.js
@@ -0,0 +1,39 @@
+var popoverlay = document.querySelector(".popup-overlay")
+var popupbox = document.querySelector(".popup-box")
+var addpopupbutton = document.getElementById("add-popup-button")
+
+addpopupbutton.addEventListener("click",function(){
+    popoverlay.style.display = "block"
+    popupbox.style.display = "block"
+})
+
+var cancelpopup = document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click",function(event){
+    event.preventDefault()
+    popoverlay.style.display = "none"
+    popupbox.style.display = "none"
+})
+
+var container = document.querySelector(".container")
+var addbook = document.getElementById("add-book")
+var booktitleinput = document.getElementById("book-title-input")
+var bookauthor = document.getElementById("book-author")
+var bookdescriptioninput = document.getElementById("book-description-input")
+
+addbook.addEventListener("click",function(){
+    event.preventDefault()
+    var div = document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML = `<h2>${booktitleinput.value}</h2>
+    <h5>${bookauthor.value}</h5>
+    <p>${bookdescriptioninput.value}</p>
+    <button onclick="deletebook(event)">Delete</button>`
+    container.append(div)
+    popoverlay.style.display = "none"
+    popupbox.style.display = "none"
+})
+
+function deletebook(event){
+    event.target.parentElement.remove()
+}
+
